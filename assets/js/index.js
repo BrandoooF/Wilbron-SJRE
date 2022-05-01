c2c9b6463df94e828b63b81832ca69f815dbecab
Brandon Fallings <brandonfallings@Brandons-Mac-mini-2.local>
2022-05-01 14:07:51 -0400

    new commit

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    //$('.carousel').slick();
+    console.log('jquery')
+
+    $("#contactForm").submit(function(e){
+        e.preventDefault();
+
+        var form = $(this)
+        var actionUrl = form.attr('action')
+
+        $('.contactSection').append('<div id="contactSuccess"><div><i class="ri-checkbox-circle-line"></i></div><p>Thanks! your message has been sent. We will be in contact with you very soon.</p></div>')
+
+    })
+
+    $('#contactSuccess').on('click', function(){
+        console.log('clicked')
+        $('#contactSuccess').remove()
+    })
+});
